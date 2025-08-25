760a5d5103c47c954e20c1b18c37f2440ac728c3
Dmitrii <legkodogatca@gmail.com>
2025-08-25 09:02:54 +0300

    edit workflowDA

--- app/database/session.py
+++ app/database/session.py
@@ -1,11 +1,33 @@
-# database/session.py
+import os
 from pathlib import Path
 from peewee import SqliteDatabase
 
-# База в каталоге database/
-DB_NAME = "Next'25.db"
-BASE_DIR = Path(__file__).resolve().parent
-DB_PATH = BASE_DIR / DB_NAME
+# имя файла по умолчанию
+DB_NAME = "Next25.db"
 
-db = SqliteDatabase(str(DB_PATH))
+# 1. Читаем путь из переменной окружения (если есть)
+db_path = os.getenv("DB_PATH")
 
+# 2. Если переменной нет → кладём рядом с кодом, как раньше
+if not db_path:
+    base_dir = Path(__file__).resolve().parent
+    db_path = base_dir / DB_NAME
+else:
+    db_path = Path(db_path)
+
+# 3. Создаём подключение
+db = SqliteDatabase(str(db_path))
+
+
+
+# # database/session.py
+# from pathlib import Path
+# from peewee import SqliteDatabase
+#
+# # База в каталоге database/
+# DB_NAME = "Next25.db"
+# BASE_DIR = Path(__file__).resolve().parent
+# DB_PATH = BASE_DIR / DB_NAME
+#
+# db = SqliteDatabase(str(DB_PATH))
+#
